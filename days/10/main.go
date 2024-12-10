01390f9722ffc70d3e00f90a222f9b69eb88b309
Isaac Naylor <i.acnaylor@gmail.com>
2024-12-10 13:13:12 +0000

    Day 10 pt1

---
+++ days/10/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func logErr(err error) {
+	fmt.Printf("ERROR: %+v\n", err)
+
+	os.Exit(1)
+}
+
+func useSample(args []string) bool {
+	if len(args) == 0 {
+		return false
+	}
+
+	if args[0] == "sample" {
+		return true
+	}
+
+	return false
+}
+
+func getInput(args []string) (string, error) {
+	fileName := "input.txt"
+	if useSample(args) {
+		fileName = "sample.txt"
+	}
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Trim(string(data), "\n"), nil
+}
+
+func main() {
+	input, err := getInput(os.Args[1:])
+
+	if err != nil {
+		logErr(err)
+	}
+
+	p1, err := part1(input)
+
+	if err != nil {
+		logErr(err)
+	}
+
+	p2, err := part2(input)
+
+	if err != nil {
+		logErr(err)
+	}
+
+	fmt.Printf("Part 1: %d\nPart 2: %d\n", p1, p2)
+}
+
+type Cell struct {
+	x, y, height int
+	reached      bool
+}
+
+type Grid struct {
+	gridSize int
+	cells    [][]*Cell
+}
+
+func newGrid(input string) (*Grid, error) {
+	gridSize := strings.Index(input, "\n")
+	cells := make([][]*Cell, 0)
+
+	for y, line := range strings.Split(input, "\n") {
+		row := make([]*Cell, 0)
+
+		for x, col := range strings.Split(line, "") {
+			height, err := strconv.Atoi(col)
+			if err != nil {
+				return nil, err
+			}
+
+			row = append(row, &Cell{x, y, height, false})
+		}
+
+		cells = append(cells, row)
+	}
+
+	return &Grid{gridSize, cells}, nil
+}
+
+func (g *Grid) reset() {
+	for _, row := range g.cells {
+		for _, cell := range row {
+			cell.reached = false
+		}
+	}
+}
+
+func (g *Grid) getCellNeighbours(c *Cell) []*Cell {
+	neighbours := make([]*Cell, 0)
+
+	coords := []struct{ x, y int }{
+		{c.x, c.y - 1},
+		{c.x - 1, c.y}, {c.x + 1, c.y},
+		{c.x, c.y + 1},
+	}
+
+	for _, coord := range coords {
+		if coord.x < 0 || coord.x >= g.gridSize || coord.y < 0 || coord.y >= g.gridSize {
+			continue
+		}
+
+		neighbours = append(neighbours, g.cells[coord.y][coord.x])
+	}
+
+	return neighbours
+}
+
+func (g *Grid) checkTrail(c *Cell) int {
+	validRoutes := 0
+
+	for _, neighbour := range g.getCellNeighbours(c) {
+		if neighbour.height == c.height+1 && neighbour.height == 9 && !neighbour.reached {
+			validRoutes++
+			neighbour.reached = true
+			continue
+		} else if neighbour.height == c.height+1 {
+			validRoutes += g.checkTrail(neighbour)
+		}
+	}
+
+	return validRoutes
+}
+
+func part1(input string) (int, error) {
+	result := 0
+
+	grid, err := newGrid(input)
+	if err != nil {
+		return -1, err
+	}
+
+	for _, row := range grid.cells {
+		for _, cell := range row {
+			if cell.height != 0 {
+				continue
+			}
+			grid.reset()
+			trails := grid.checkTrail(cell)
+
+			result += trails
+		}
+	}
+
+	return result, nil
+}
+
+func part2(input string) (int, error) {
+	result := 0
+
+	return result, nil
+}
